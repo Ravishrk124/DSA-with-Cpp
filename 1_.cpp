7e58b3098d987dae12310ff2122ebef653f64685
Ravish Kumar <ravishrk124@gmail.com>
2025-09-26 00:25:13 +0530

    Day 1: Hello World program

---
+++ 1_.cpp
@@ -0,0 +1,8 @@
+#include<iostream>  // Include input-output stream library (for cout, cin)
+using namespace std; // Use standard namespace to avoid writing std:: everywhere
+
+// Main function: program execution starts from here
+int main() {
+    cout << "Hello World" << endl; // Print "Hello World" and go to next line
+    return 0; // Return 0 to indicate successful execution
+}
